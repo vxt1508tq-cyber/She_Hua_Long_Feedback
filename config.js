22ef1f69da6cd74c23c6e13000a3eedac344f54b
vxt1508tq-cyber <vxt1508tq@gmail.com>
2026-08-12 20:10:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Replace these values with your own project values before deployment.
 window.SHE_HUA_LONG_CONFIG = {
   supabaseUrl: "https://ogfngvizksjjvgbvhzjl.supabase.co",
-  supabasePublishableKey: "PASTE_YOUR_PUBLISHABLE_KEY_HERE",
+  supabasePublishableKey: "sb_publishable_aEVWkHBzbWbzusvmZDCcLw_WasXmRS8",
   googleMapsUrl: "https://maps.app.goo.gl/YtP8Dwm1LrDLKzMe7?g_st=ic"
 };
